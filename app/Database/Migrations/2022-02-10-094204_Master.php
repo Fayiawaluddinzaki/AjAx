bec77bfca058368afb7a06a9f1a0859e5cb8455a
Fayi Awaluddin Zaki <zakiawaluddin@gmail.com>
2022-02-10 16:44:56 +0700

    push

---
+++ app/Database/Migrations/2022-02-10-094204_Master.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Master extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'master_id'=>[
+                'type'=>'INT',
+                'constraint'=>11,
+                'unsigned'=>true,
+                'auto_increment'=>true,
+            ],
+            'username_id'=>[
+                'type'=>'INT',
+                'constraint'=>11,
+                'unsigned'=>true,
+            ],
+            'kode_entry'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+            ],
+            'jam_masuk'=>[
+                'type'=>'TIME',
+            ],
+            'jam_keluar'=>[
+                'type'=>'TIME',
+            ],
+            'barcode'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+            ],
+            'created_time time default current_timestamp',
+            'created_date date default current_timestamp',
+        ]);
+        $this->forge->addKey('master_id',true);
+        $this->forge->createTable('master');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('master');
+    }
+}
